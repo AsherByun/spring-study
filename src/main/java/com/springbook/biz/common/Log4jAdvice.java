4ea565777e6d98a0685cdc98e01cd4567753627a
변성재 <dfdo333@naver.com>
2020-12-28 16:57:13 +0900

    jdbc추가

--- src/main/java/com/springbook/biz/common/Log4jAdvice.java
+++ src/main/java/com/springbook/biz/common/Log4jAdvice.java
@@ -1,7 +1,7 @@
 package com.springbook.biz.common;
 
 public class Log4jAdvice {
-	public void printLog() {
-		System.out.println("[공통 로그 log4j] 비즈니스 로직 수행 전 동작");
+	public void printLogging() {
+		System.out.println("[공통 로그-Log4j] 비즈니스 로직 수행 전 동작");
 	}
 }
